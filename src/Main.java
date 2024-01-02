ff7089b88b3e721dea5d8b3c157e442bcdf217be
eetnaviation <vindipesa@gmail.com>
2024-01-02 13:33:30 +0200

    Added credit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import gui.pointRenderer;
 public class Main {
     public static void main(String[] args) {
         //pointRenderer.main();
+        System.out.println("kaler v1.0 by eetnaviation");
         IpScanner.main("192.168.1.0", 255, 1500, false);
     }
 }
