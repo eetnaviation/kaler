da58931670299fb267b543e81c6bd86753902286
eetnaviation <vindipesa@gmail.com>
2024-01-02 13:31:31 +0200

    Disabled debugArray by default

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import gui.pointRenderer;
 public class Main {
     public static void main(String[] args) {
         //pointRenderer.main();
-        IpScanner.main("192.168.1.0", 255, 1500, true);
+        IpScanner.main("192.168.1.0", 255, 1500, false);
     }
 }
